a88074ee7612a6f6cb8fe7017fd06de2db50fafd
tarikklckya <trkklckya@gmail.com>
2022-09-07 19:12:53 +0300

    ilk version

---
+++ src/test/java/Ilkclass.java
@@ -0,0 +1,10 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Ilkclass {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","src/resources/drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.techproeducation.com");
+    }
+}
